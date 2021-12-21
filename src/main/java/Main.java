926bd3c5ea549359a60212ee27e1356e5cfea651
Savser91 <savser91@gmail.com>
2021-12-21 22:28:56 +0300

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,10 @@
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.*;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.*;
-
-import java.io.IOException;
+import java.io.File;
+import java.io.*;
 import java.util.List;
 
 public class Main {
@@ -22,8 +22,21 @@ public class Main {
 
         HttpGet request = new HttpGet(REMOTE_SERVICE_URL);
         CloseableHttpResponse response = httpClient.execute(request);
-
+        mapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
         List<ApiNASA> apiNASAList = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<ApiNASA>>() {});
+        String imageUrl = apiNASAList.get(0).getUrl();
+        String imageName = imageUrl.substring(imageUrl.lastIndexOf("/") + 1, imageUrl.length());
+        request = new HttpGet(imageUrl);
 
+        saveFile(httpClient.execute(request), imageName);
+    }
+
+    public static void saveFile(CloseableHttpResponse response, String imageName) throws IOException {
+        byte[] imgBytes = response.getEntity().getContent().readAllBytes();
+        try (FileOutputStream fos = new FileOutputStream(new File(imageName))) {
+            fos.write(imgBytes, 0, imgBytes.length);
+        } catch (Exception exception) {
+            exception.getMessage();
+        }
     }
 }
